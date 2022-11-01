e27b8a40caa4fbb78ed8d0dd8966545ad2e5ffed
lane-franklin <90738586+lane-chembel@users.noreply.github.com>
2022-10-31 21:19:48 -0700

    add requested functionality

--- test/test_app.py
+++ test/test_app.py
@@ -13,7 +13,13 @@ class FlaskTest(unittest.TestCase):
         response = tester.get("/getSomeData")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-
+    # check that employee data is formatted correctly 
+    def test_data(self):
+       tester = app.app.test_client(self)
+       response = tester.get("/getEmployees")
+       keys = response[0].keys()
+       self.assertIn('name', keys)
+       self.assertIn('level of effort', keys)
 
 if __name__ == "__main__":
     unittest.main()

e3b17834e919cfc18b43d31ddcaa98d8d100eb24
Laney Franklin <Helena.C.Franklin@jpl.nasa.gov>
2022-11-01 11:04:50 -0700

    update .gitignore and add python unit tests

--- test/test_app.py
+++ test/test_app.py
@@ -1,25 +1,32 @@
 import sys
+
 sys.path.append('.')
 sys.path.append('..')
 
-
 import unittest
 import app
+
+
+# check response 200 from each
 class FlaskTest(unittest.TestCase):
 
-    # check response 200
-    def test_index(self):
+    def test_get_some_data(self):
         tester = app.app.test_client(self)
         response = tester.get("/getSomeData")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-    # check that employee data is formatted correctly 
-    def test_data(self):
-       tester = app.app.test_client(self)
-       response = tester.get("/getEmployees")
-       keys = response[0].keys()
-       self.assertIn('name', keys)
-       self.assertIn('level of effort', keys)
+
+    def from_file(self):
+        tester = app.app.test_client(self)
+        response = tester.get("/getSomeDataFromFile")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_get_employees(self):
+        tester= app.app.test_client(self)
+        response = tester.get("/getEmployees")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
 
 if __name__ == "__main__":
     unittest.main()
